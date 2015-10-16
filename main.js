e863025386c47df6bfdb87a8732080e3161c9fe2
Isaac <icortes95@live.com>
2015-10-16 16:21:18 -0500

    Testing things

---
+++ main.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Isaac on 10/16/15.
+ */
+
+
+var locationNames = ["Hills", "Forest", "Mountains", "Fields", "Pasture", "Dessert"];
+
+//location card class
+function Location(locationName, number)  {
+    //constructor
+    this.locationName = locationName;
+    this.number = number;
+
+    //methods
+    this.getlocationName = function() {return this.locationName};
+    this.getnumber = function() {return this.number};
+}
+//var isaac = new Location("Isaac", 20);
+//var emily = new Location("Emily", 20)
+var locations = [];
+for (var i in locationNames.length) {
+    locations.push(new Location(locationNames[i], 2));
+}
+
+//locations.push(new Location("Isaac",20));
+
+console.log(locations);
